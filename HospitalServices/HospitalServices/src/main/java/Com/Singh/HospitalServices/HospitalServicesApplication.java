b2b9e231ba6e46db9bc581495320885d474ae98a
vishvjeet sing panwar <visvhjeetrajput0000@gmail.com>
2025-08-06 17:25:42 +0530

    project initial setup

---
+++ HospitalServices/HospitalServices/src/main/java/Com/Singh/HospitalServices/HospitalServicesApplication.java
@@ -0,0 +1,13 @@
+package Com.Singh.HospitalServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalServicesApplication.class, args);
+	}
+
+}
---
+++ HospitalServices/HospitalServices/src/test/java/Com/Singh/HospitalServices/HospitalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package Com.Singh.HospitalServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
